3a83bba2ae351dd314404fda44696210a8045ad5
hit9 <nz2324@126.com>
2012-10-30 05:46:21 +0800

    mkdwiki

---
+++ blog-src/dir_htmls.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#--coding:utf-8--
+import os, re, glob
+
+def tree(d, fi):
+	if os.path.basename(d) == '.git':
+		return 
+	for i in os.listdir(d):
+		p = os.path.join(d, i)
+		if os.path.isdir(p) :
+			tree(p, fi)
+		elif re.match('.*\.html$', os.path.basename(p)):
+			f = open(p)
+			c = f.read()
+			t= title_re.search(c)
+			title=t.group(1).strip() if t else os.path.basename(p)
+			f.close()
+			fi.write('1. '+'['+title+']('+os.path.relpath(p, '..')+')\n')
+title_re = re.compile(r'<title>(.*)</title>')
+fi = open('dir_html.mkd', 'w')
+fi.write('%title dir_html')
+fi.write('###Ctrl+F查找文章\n\n')
+tree('..', fi)
+fi.close()
+print 'dir html list ok.'
