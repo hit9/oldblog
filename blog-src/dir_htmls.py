d489b585cef781322560cc55468dc7b94fab29c8
hit9 <nz2324@126.com>
2012-12-07 22:54:00 +0800

    retab

--- blog-src/dir_htmls.py
+++ blog-src/dir_htmls.py
@@ -3,19 +3,19 @@
 import os, re, glob
 
 def tree(d, fi):
-	if os.path.basename(d) == '.git':
-		return 
-	for i in os.listdir(d):
-		p = os.path.join(d, i)
-		if os.path.isdir(p) :
-			tree(p, fi)
-		elif re.match('.*\.html$', os.path.basename(p)):
-			f = open(p)
-			c = f.read()
-			t= title_re.search(c)
-			title=t.group(1).strip() if t else os.path.basename(p)
-			f.close()
-			fi.write('1. '+'['+title+']('+os.path.relpath(p, '..')+')\n')
+    if os.path.basename(d) == '.git':
+        return 
+    for i in os.listdir(d):
+        p = os.path.join(d, i)
+        if os.path.isdir(p) :
+            tree(p, fi)
+        elif re.match('.*\.html$', os.path.basename(p)):
+            f = open(p)
+            c = f.read()
+            t= title_re.search(c)
+            title=t.group(1).strip() if t else os.path.basename(p)
+            f.close()
+            fi.write('1. '+'['+title+']('+os.path.relpath(p, '..')+')\n')
 title_re = re.compile(r'<title>(.*)</title>')
 fi = open('dir_html.mkd', 'w')
 fi.write('%title Archives\n')
